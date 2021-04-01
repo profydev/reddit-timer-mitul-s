f98c41915b00bc38fcd70373e9c2b224c1a1ff2b
Mitul Shah <mitulxshah@gmail.com>
2021-04-01 18:28:03 +0000

    set up routes, added new pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import { 
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+import Home from './pages/Home';
+import Search from './pages/Search';
+
+
+
 
 function App() {
   return (
-    <div>
-     initial commit
-    </div>
+     <Router>
+       <Switch>
+         <Route exact path="/">
+          <Home />
+         </Route>
+         <Route path="/search">
+          <Search />
+         </Route>
+       </Switch>
+     </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import 'styles/global.css';
 import 'normalize.css';
 
 // eslint-disable-next-line react/jsx-filename-extension
