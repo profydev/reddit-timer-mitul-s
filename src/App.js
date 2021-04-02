a085ae7445ab89fc983825234dda9b0c282fceda
Mitul Shah <mitulxshah@gmail.com>
2021-04-02 11:06:02 -0400

    Create app skeleton (#1)
    * initial commit test
    
    * added react-router dom and normalize css
    
    * added fonts to index
    
    * set up routes, added new pages
    
    * fixed linting errors

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+import Home from './pages/Home';
+import Search from './pages/Search';
 
 function App() {
   return (
-    <div>
-      App Placeholder
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/search">
+          <Search />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './styles/global.css';
+import 'normalize.css';
 
 // eslint-disable-next-line react/jsx-filename-extension
 ReactDOM.render(<App />, document.getElementById('root'));
